73d55e965c7b1f0088e5a8556420ba48340c5bae
Marvin Ferber <ferbhome@googlemail.com>
2023-02-15 09:59:01 +0100

    added function bin2hex

--- main.py
+++ main.py
@@ -1,22 +1,71 @@
+länge = 0
+index = 0
+c = ""
+four = ""
+ret = ""
 ergebnis = ""
 rest = 0
-zahl = 0
+zahl2 = 0
 binstring = ""
-def dez2bin(zahl2: number):
+def bin2hex(bin1: str):
+    global länge, c, four, ret, index
+    länge = len(bin1) / 4
+    while index <= länge:
+        c = ""
+        four = bin1.substr(index * 4, 4)
+        if four == "0000":
+            c = "0"
+        elif four == "0001":
+            c = "1"
+        elif four == "0010":
+            c = "2"
+        elif four == "0011":
+            c = "3"
+        elif four == "0100":
+            c = "4"
+        elif four == "0101":
+            c = "5"
+        elif four == "0110":
+            c = "6"
+        elif four == "0111":
+            c = "7"
+        elif four == "1000":
+            c = "8"
+        elif four == "1001":
+            c = "9"
+        elif four == "1010":
+            c = "A"
+        elif four == "1011":
+            c = "B"
+        elif four == "1100":
+            c = "C"
+        elif four == "1101":
+            c = "D"
+        elif four == "1110":
+            c = "E"
+        elif four == "1111":
+            c = "F"
+        ret = "" + ret + c
+        index += 1
+    return ret
+def dez2bin(zahl22: number):
     global ergebnis, rest
     ergebnis = ""
     while True:
         # Division mit Rest, speichere nur den Rest
-        rest = zahl2 % 2
-        zahl2 = Math.idiv(zahl2, 2)
+        rest = zahl22 % 2
+        # Division mit Rest, speichere den ganzzahligen Quotient
+        zahl22 = Math.idiv(zahl22, 2)
+        # füge dem Ergebnis ein weiteres Bit rechts hinzu
         ergebnis = "" + str(rest) + ergebnis
-        if zahl2 == 0:
+        # Falls die Zahl 0 geworden ist, ist die Rechnung beendet
+        if zahl22 == 0:
             break
     return ergebnis
 
 def on_forever():
     global binstring
-    binstring = dez2bin(8)
+    binstring = bin2hex("11000110")
     basic.show_string(binstring)
     basic.pause(2000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,25 +1,80 @@
+let länge = 0
+let index = 0
+let c = ""
+let four = ""
+let ret = ""
 let ergebnis = ""
 let rest = 0
-let zahl = 0
+let zahl2 = 0
 let binstring = ""
-function dez2bin (zahl: number) {
+function bin2hex(bin1: string): string {
+    
+    länge = bin1.length / 4
+    while (index <= länge) {
+        c = ""
+        four = bin1.substr(index * 4, 4)
+        if (four == "0000") {
+            c = "0"
+        } else if (four == "0001") {
+            c = "1"
+        } else if (four == "0010") {
+            c = "2"
+        } else if (four == "0011") {
+            c = "3"
+        } else if (four == "0100") {
+            c = "4"
+        } else if (four == "0101") {
+            c = "5"
+        } else if (four == "0110") {
+            c = "6"
+        } else if (four == "0111") {
+            c = "7"
+        } else if (four == "1000") {
+            c = "8"
+        } else if (four == "1001") {
+            c = "9"
+        } else if (four == "1010") {
+            c = "A"
+        } else if (four == "1011") {
+            c = "B"
+        } else if (four == "1100") {
+            c = "C"
+        } else if (four == "1101") {
+            c = "D"
+        } else if (four == "1110") {
+            c = "E"
+        } else if (four == "1111") {
+            c = "F"
+        }
+        
+        ret = "" + ret + c
+        index += 1
+    }
+    return ret
+}
+
+function dez2bin(zahl22: number): string {
+    
     ergebnis = ""
     while (true) {
-        // Division mit Rest, speichere nur den Rest
-        rest = zahl % 2
-        // Division mit Rest, speichere den ganzzahligen Quotient
-        zahl = Math.idiv(zahl, 2)
-        // füge dem Ergebnis ein weiteres Bit rechts hinzu
-        ergebnis = "" + rest + ergebnis
-        // Falls die Zahl 0 geworden ist, ist die Rechnung beendet
-        if (zahl == 0) {
-            break;
+        //  Division mit Rest, speichere nur den Rest
+        rest = zahl22 % 2
+        //  Division mit Rest, speichere den ganzzahligen Quotient
+        zahl22 = Math.idiv(zahl22, 2)
+        //  füge dem Ergebnis ein weiteres Bit rechts hinzu
+        ergebnis = "" + ("" + rest) + ergebnis
+        //  Falls die Zahl 0 geworden ist, ist die Rechnung beendet
+        if (zahl22 == 0) {
+            break
         }
+        
     }
     return ergebnis
 }
-basic.forever(function () {
-    binstring = dez2bin(8)
+
+basic.forever(function on_forever() {
+    
+    binstring = bin2hex("11000110")
     basic.showString(binstring)
     basic.pause(2000)
 })
